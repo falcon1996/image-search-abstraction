02991a5b8e2ae91c353238173519f08a835b9841
Dhruv Parashar <dhruvparashar6@gmail.com>
2017-06-06 16:26:37 +0000

    mongoose added

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 var express = require('express');
-var validUrl = require('valid-url');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 var request = require('request');
+var Bing = require('node-bing-api')({ accKey: "" });
+
 var mongo = require('mongodb').MongoClient;
-const googleIms = require('google-ims');
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
-var client = new googleIms('', '');
+
+var historySchema = new Schema({
+    term: String,
+    when: String
+});
+
+var History = mongoose.model('History', historySchema);
 
 
 var murl = 'mongodb://localhost:27017/url-shortner';
+mongoose.connect(murl);
+
 
 app.get('/', function(req, res){
     res.render('index.html');
@@ -22,59 +32,60 @@ app.get('/search', function(req, res) {
     var query = req.query.q;
     var offset = req.query.offset;          //for https://img-search-api-bq.herokuapp.com/api/search?q=cats&offset=10
     
-    var input = '/search?q='+query+'&offset='+offset;
     
-    
-    mongo.connect(murl,function(err,db){
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function() {
         
-        if(err) throw err;
         
-        var collection = db.collect('myList');
-        collection.insert({history:input},function(err, doc){
+        
+        var history = History({
             
-            if(err) throw err;
-            
-            console.log(JSON.stringify(doc))
-            db.close()
-            
-        })
-        
-    })
-    
-    
-    client.search(query, {
-        
-        page : offset,
-        size : 'large',
-        safe : 'off',
-        fileType : 'jpg',
-        googlehost: 'google.com',
-        num : 1
-        
-    }).then(function(images){
-        
-        images.forEach(function(i, e, a){
-            
-            res.end(images);
+            "term": query,
+            "when": new Date().toLocaleString()
         });
+        
+        
+        history.save(function (err){
+            
+            if (err) throw err;
+            
+            console.log('Working!');
+        });
+        
+    });
+
+    
+    
+    Bing.images(query,{
+        
+        top : offset,
+        skip : 0               // max can be 10
+    }, function(err, resp, body){
+        
+        res.end(JSON.stringify(body));
     });
     
-    //res.end('Working!!!!!!!!!!!');
     
 });
 
 
 app.get('/latest', function(req, res){
     
-    mongo.connect(murl, function(err, db){
-        
-        if(err) throw err;
-        
-        db.listCollections();
-    })
     
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function(){
+        
+        History.find({},function(err, myhistory){
+            
+            if (err)  throw(err);
+            
+            res.end(myhistory);
+        });
+    });
     
-})
+});
 
 
 
